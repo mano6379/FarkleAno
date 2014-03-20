458e59a2305b1afa1772c980d7c07dad6db2ff1f
Marion Ano <meaola@gmail.com>
2014-03-19 19:13:17 -0500

    first commit

---
+++ FarkleAno/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FarkleAno
+//
+//  Created by Marion Ano on 3/19/14.
+//  Copyright (c) 2014 Marion Ano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
